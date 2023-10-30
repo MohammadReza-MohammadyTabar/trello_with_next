5ba404ec710875d800fa2efe26a008574d928d2d
O mohammaditabar <o.mohammady@ofogh.com>
2023-10-30 17:12:50 +0330

    add header component and ai

---
+++ components/Header.tsx
@@ -0,0 +1,57 @@
+'use client'
+import Image from 'next/image'
+import React from 'react'
+import { MagnifyingGlassIcon, UserCircleIcon } from '@heroicons/react/20/solid'
+import Avatar from 'react-avatar'
+function Header() {
+  return (
+    <header>
+      <div className='flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl'>
+        <div className='
+        absolute
+        top-0
+        left-0
+        w-full
+        h-96
+        bg-gradient-to-br
+        from-pink-400
+        to-blue-900
+        rounded-md
+        -z-50
+        opacity-60
+        filter 
+        blur-3xl
+        ' />
+        <Image
+          src='https://upload.wikimedia.org/wikipedia/en/thumb/8/8c/Trello_logo.svg/1280px-Trello_logo.svg.png'
+          alt='log'
+          width={300}
+          height={100}
+          className='w-44 md:w-56 pb-10 md:pb-0 object-contain'
+        />
+        <div className='flex items-center space-x-5 flex-1 justify-end w-full'>
+          {/* search box */}
+          <form className='flex items-center space-x-4 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial'>
+            <MagnifyingGlassIcon className='h-6 w-6 text-gray-400' />
+            <input type="text" placeholder='Search' className='flex-1 outline-none p-2' />
+            <button type='submit' hidden>search</button>
+          </form>
+
+        {/* Avatar */}
+
+        <Avatar name='mohammad' round size='50'/>
+        </div>
+        
+      </div>
+
+      <div className='flex items-center justify-center px-5 md:py-5'>
+        <p className='flex items-center text-sm font-light p-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-blue-500'>
+          <UserCircleIcon className='inline-block h-10 w-10 mr-1 text-blue-700' />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        </p>
+      </div>
+    </header>
+  )
+}
+
+export default Header
