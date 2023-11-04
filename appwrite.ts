40b1687cd86ebbeb46b68aef2f1def55d5faf841
O mohammaditabar <o.mohammady@ofogh.com>
2023-11-04 16:51:06 +0330

    connect to appwrite

--- appwrite.ts
+++ appwrite.ts
@@ -1,10 +1,11 @@
 import { ID,Account, Client, Databases ,Storage} from 'appwrite';
+import {APPWRITE_PROJECT_ID} from '@/lib/configs'
 
 const client = new Client();
 
 client
     .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject(process.env.APPWRITE_PROJECT_ID!);
+    .setProject(APPWRITE_PROJECT_ID!);
 
 const account =new Account(client)
 const database=new Databases(client)
